d28d7f41a30ef8cfe105a63844fba8df437e2933
haruna.iizuka <dubrovnik302@gmail.com>
2018-06-19 04:36:33 +0000

    signup fix2

--- routes/web.php
+++ routes/web.php
@@ -32,12 +32,5 @@ Route::delete('logout', 'Auth\LoginController@logout')->name('logout.delete');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
-    Route::group(['prefix' => 'users/{id}'], function () {
-        Route::post('follow', 'UserFollowController@store')->name('user.follow');
-        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
-        Route::get('followings', 'UsersController@followings')->name('users.followings');
-        Route::get('followers', 'UsersController@followers')->name('users.followers');
-    });
-
-    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+    Route::resource('tasks', 'TasksController', ['only' => ['store', 'destroy']]);
 });
